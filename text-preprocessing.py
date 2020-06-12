1d21eb36f555d0b40cae58bd377ef98e1ad09bb3
Ryan Rusli <ryanrusli1407@yahoo.com>
2020-06-12 17:06:06 +0700

    text processing for indonesian

--- text-preprocessing.py
+++ text-preprocessing.py
@@ -1,26 +1,28 @@
 # library to stem in Indonesian
-# import Sastrawi
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 # library for stemming and tokenizing
-from nltk.stem import PorterStemmer 
+# from nltk.stem import PorterStemmer 
 from nltk.tokenize import word_tokenize 
 # library to generate TD-IDF
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # string input for text
-# Removes prefixes, suffixes, punctuation 
+# Removes prefixes, suffixes, punctuation 	
 # Converts all text to lower case
 # returns TF-IDF of the text
 def preprocess(text):
 	token_list = []
 	vectorizer = TfidfVectorizer()
-	ps = PorterStemmer()
+	factory = StemmerFactory()
+	ps = factory.create_stemmer()
 	processed_text = ' '.join(ps.stem(token) for token in word_tokenize(text))
 	token_list.append(processed_text)
 	x = vectorizer.fit_transform(token_list)
+	print(token_list)
 	print(x)
 	return x
 
 #Example 
-preprocess("GO go! makan dimakan go! going go go GO GO going")
+preprocess("dimakan makan memakan")
